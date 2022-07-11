8d4722507c8f6c2e22ff04a614d5a987b9dfb5c5
Saptarshi Datta <saptarshidutta056@gmail.com>
2022-07-11 18:39:53 +0530

    Few more changes to projects page

--- src/project.js
+++ src/project.js
@@ -2,6 +2,9 @@ import React from 'react'
 import styles from './Styles/styleguide.module.css';
 import design from './Styles/project.module.css';
 import {Row, Col} from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faExternalLinkSquareAlt } from '@fortawesome/free-solid-svg-icons';
 
 const Project = () => {
     return (<>
@@ -11,14 +14,18 @@ const Project = () => {
             <Col md={6} className="px-md-0 pr-md-0">
             <img src="/images/royal.png" alt='aaaa' className={design.image}></img>
             </Col>
-            <Col md={6} className="px-md-0 pr-md-0">
+            <Col md={6}>
                 <div>
                     <p className={design.type}>Freelance Project</p>
                     <h3 className={design.type}>Royal Desino</h3>
                     <p className={["px-4 py-3 ", design.detail].join('')}>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. In facilisis venenatis ipsum. Etiam scelerisque tempor enim, ac lacinia nulla aliquet id. Donec nibh nisi, blandit ut eleifend vel, suscipit vitae magna.
                     </p>
-                </div>
+                    <p style={{display: "flex", flexDirection: "row-reverse"}}>
+                        <a href="http://royal-desino.herokuapp.com/"><FontAwesomeIcon icon={faExternalLinkSquareAlt} style={{ color: "grey", fontSize: "25px", marginLeft: "30px"}} /><br /></a>
+                        <a href="http://royal-desino.herokuapp.com/"><FontAwesomeIcon icon={faGithub} style={{ color: "pink", fontSize: "25px"}} /><br /></a>
+                    </p>
+                    </div>
             </Col>
         </Row>
         <Row className={["d-flex flex-row-reverse article", design.article].join('')}>
@@ -26,9 +33,9 @@ const Project = () => {
             <img src="/images/royal.png" alt='aaaa' className={design.image}></img>
             </Col>
             <Col md={6} right className="px-md-0 pr-md-0">
-                <div>
-                    <p className={design.type}>Freelance Project</p>
-                    <h3 className={design.type}>Royal Desino</h3>
+                <div className={design.altBlock}>
+                    <p className={design.type} style={{"textAlign":"left"}}>Freelance Project</p>
+                    <h3 className={design.type} style={{"textAlign":"left"}}>Royal Desino</h3>
                     <p className={["px-4 py-3 ", design.detail].join('')}>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. In facilisis venenatis ipsum. Etiam scelerisque tempor enim, ac lacinia nulla aliquet id. Donec nibh nisi, blandit ut eleifend vel, suscipit vitae magna.
                     </p>
@@ -39,7 +46,7 @@ const Project = () => {
             <Col md={6} left className="px-md-0 pr-md-0">
             <img src="/images/royal.png" alt='aaaa' className={design.image}></img>
             </Col>
-            <Col md={6} className="px-md-0 pr-md-0">
+            <Col md={6}>
                 <div>
                     <p className={design.type}>Freelance Project</p>
                     <h3 className={design.type}>Royal Desino</h3>
